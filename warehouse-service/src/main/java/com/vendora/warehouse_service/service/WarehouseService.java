9bf277ec13094bc25ebd1f6b53beeb4ec05a26ce
Cryobs <cryobs.ads@gmail.com>
2025-01-06 10:31:00 +0100

    Add ProductUnavailableException anf handle it in WarehouseService.addStockToProduct;

--- warehouse-service/src/main/java/com/vendora/warehouse_service/service/WarehouseService.java
+++ warehouse-service/src/main/java/com/vendora/warehouse_service/service/WarehouseService.java
@@ -2,6 +2,7 @@ package com.vendora.warehouse_service.service;
 
 import com.vendora.warehouse_service.entity.ProductEntity;
 import com.vendora.warehouse_service.entity.WarehouseEntity;
+import com.vendora.warehouse_service.exception.ProductUnavailableException;
 import com.vendora.warehouse_service.repository.ProductRepo;
 import com.vendora.warehouse_service.repository.WarehouseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +22,20 @@ public class WarehouseService {
     @Autowired
     private ProductService productService;
 
-    public WarehouseEntity addStockToProduct(Long productId, int additionalQuantity){
+    public WarehouseEntity addStockToProduct(Long productId, int additionalQuantity) throws ProductUnavailableException {
         Optional<WarehouseEntity> optionalWarehouse = warehouseRepo.findByProductId(productId);
 
         if(optionalWarehouse.isPresent()){
             WarehouseEntity warehouse = optionalWarehouse.get();
-            warehouse.setStockQuantity(warehouse.getStockQuantity() + additionalQuantity);
-            return warehouseRepo.save(warehouse);
+            //check if unavaible
+            if(warehouse.getStockQuantity() + additionalQuantity >= 0 ) {
+                warehouse.setStockQuantity(warehouse.getStockQuantity() + additionalQuantity);
+                return warehouseRepo.save(warehouse);
+            } else {
+                throw new ProductUnavailableException("Product with ID " + productId + " unavailable" +
+                        (warehouse.getStockQuantity() != 0 ?  " with this quantity, available only " + warehouse.getStockQuantity() : "")
+                );
+            }
         } else {
             Optional<ProductEntity> optionalProduct = productRepo.findById(productId);
             //register product in warehouse if absent
