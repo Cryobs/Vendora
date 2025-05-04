43d32dc716186ee1143bed3b6e169ee8f49e14ef
cryobs <cryobs.ads@gmail.com>
2025-05-04 11:15:27 +0200

    Notifications-service refactor with lombok

--- notification-service/src/main/java/com/vendora/notification_service/DTO/OrderItemDTO.java
+++ notification-service/src/main/java/com/vendora/notification_service/DTO/OrderItemDTO.java
@@ -1,8 +1,13 @@
 package com.vendora.notification_service.DTO;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
 public class OrderItemDTO {
     private UUID id;
 
@@ -14,63 +19,4 @@ public class OrderItemDTO {
     private BigDecimal totalTax;
 
     private BigDecimal finalPrice;
-
-    public OrderItemDTO() {
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    public UUID getProductId() {
-        return productId;
-    }
-
-    public void setProductId(UUID productId) {
-        this.productId = productId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public BigDecimal getTotalDiscount() {
-        return totalDiscount;
-    }
-
-    public void setTotalDiscount(BigDecimal totalDiscount) {
-        this.totalDiscount = totalDiscount;
-    }
-
-    public BigDecimal getTotalTax() {
-        return totalTax;
-    }
-
-    public void setTotalTax(BigDecimal totalTax) {
-        this.totalTax = totalTax;
-    }
-
-    public BigDecimal getFinalPrice() {
-        return finalPrice;
-    }
-
-    public void setFinalPrice(BigDecimal finalPrice) {
-        this.finalPrice = finalPrice;
-    }
 }
